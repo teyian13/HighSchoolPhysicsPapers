53a6a373181fb0ef890d81732a9b6e5d68ed494e
teyian13 <teyian13@outlook.com>
2024-04-15 12:59:42 +0800

    2021

---
+++ docx2md.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+
+# 请替换为你的文件所在文件夹路径
+directory = '高考备考/高考真题/2021/'
+
+for filename in os.listdir(directory):
+    if filename.endswith(".docx"):
+        md_filename = os.path.splitext(filename)[0] + '.md'
+        subprocess.run(['pandoc', '-s', os.path.join(directory, filename), '-t', 'markdown', '-o', os.path.join(directory, md_filename)])
+        os.remove(os.path.join(directory, filename)) # 这行代码将删除.docx文件
