beb903c89ef3dd62a8e5dac28eeec5527e520e88
teyian13 <teyian13@outlook.com>
2024-04-15 14:47:36 +0800

    2021

--- md_format.py
+++ md_format.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 directory = '高考备考/高考真题/2021/'
 
@@ -23,8 +24,22 @@ for filename in os.listdir(directory):
         content = content.replace('（  ）', '$（\qquad）$')
         content = content.replace('left（', 'left(')
         content = content.replace('right）', 'right)')
+        content = content.replace('[          ]{.underline}', '____')
+        content = content.replace('$$', '$')
 
 
+        # +空格
+        content = content.replace('\cdot', '\cdot ')
+        content = content.replace('\sin', '\sin ')
+        content = content.replace('\mu', '\mu ')
+        content = content.replace('\geq', '\geq ')
+        content = content.replace('\Delta', '\Delta ')
+        content = content.replace('\pi', '\pi ')
+        content = content.replace('\omega', '\omega ')
+        content = content.replace('#####', '##### ')
+
+        # text mathrm
+        content = re.sub(r'\\text\{([^a-zA-Z0-9\u4E00-\u9FA5\s]+)\}', r'\\mathrm{\1}', content)
         # 把处理后的字符串写回文件
         with open(filepath, 'w', encoding='utf-8') as md_file:
             md_file.write(content)
