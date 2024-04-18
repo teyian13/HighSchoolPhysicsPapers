92d08813c0a7922fc73ec5afcb4f859ae0d6f409
teyian13 <teyian13@outlook.com>
2024-04-18 15:03:28 +0800

    23

--- md_format.py
+++ md_format.py
@@ -34,16 +34,15 @@ for filename in os.listdir(directory):
         content = content.replace( '）',')',)
 
         # mathpix
-        content = content.replace('$$', '$')
         content = content.replace('\n$\n', '\n$$\n')
         content = content.replace('\\text{', '\\mathrm{')
-        content = re.sub(r'\$([ABCD]+)\$', r'\1', content)
-        content = content.replace('Ω', '\ohm')
+        content = re.sub(r'\$([ABCDE]+)\$', r'\1', content)
+        content = content.replace('Ω', '\Omega')
         content = content.replace('\mathrm{/}', '/')
         content = content.replace('°', '^\\circ')
 
         # +空格
-        keywords_for_space_addition = ['\\cdot', '\\sin', '\\sim', '\\mu', '\\geq', '\\leq', '\\Delta', '\\pi', '\\omega', '\\times', '\\rho', '\\approx', '\\rightarrow', '\\propto','\\triangle','\\angle']
+        keywords_for_space_addition = ['\\cdot', '\\sin', '\\sim', '\\mu', '\\geq', '\\leq', '\\Delta', '\\pi', '\\omega', '\\times', '\\rho', '\\approx', '\\rightarrow', '\\propto','\\triangle','\\angle','\\hline']
 
         for keyword in keywords_for_space_addition:
             content = content.replace(keyword, keyword + ' ')
@@ -62,7 +61,6 @@ for filename in os.listdir(directory):
 
 
         content = re.sub(r"\$.*?\$", lambda x: ' ' + x.group() + ' ', content)
-        content = content.replace(' $$ ', '$$')
 
         # 仅在行首题号后添加空格
         # '^' 代表行首， '\d{1,2}' 匹配1到2位的数字
@@ -73,6 +71,8 @@ for filename in os.listdir(directory):
         # 在每两道题之间添加空行
         # 注意，此处的 "\n" 其实是为了匹配上一题的结束，所以我们要保留它，即替换为 "\n\n" （两个换行符）
         content = re.sub(r'(\n)(\d{1,2}\. )', r'\n\1\2', content)
+
+        content = re.sub(r'^\s*[\w\s]*。\s*', lambda m: m.group().replace('。', '.'), content, flags=re.M)  
 
         # 把处理后的字符串写回文件
         with open(filepath, 'w', encoding='utf-8') as md_file:
